f56937ea2b1b40cc1b12dfbe985dc4c42ab8318a
Thomas Dillard <thomas.dillard.studios@gmail.com>
2020-06-07 01:47:07 -0700

    Added a basic Layout component including sidebar.

---
+++ packages/client/src/components/ui/Profile.svelte
@@ -0,0 +1,35 @@
+<script>
+	import User32 from 'carbon-icons-svelte/lib/User32';
+
+	export let link;
+	export let img;
+</script>
+
+<div class="flex-shrink-0 flex border-t border-gray-200 p-4">
+	<a href={link} class="flex-shrink-0 group block">
+		<div class="flex items-center">
+			<div class="h-9 w-9 rounded-full bg-gray-200 flex justify-center items-center">
+				{#if img}
+					<img
+						class="inline-block h-9 w-9 rounded-full"
+						src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+						alt="" />
+				{:else}
+					<User32 />
+				{/if}
+			</div>
+			<div class="ml-3">
+				<p
+					class="text-sm leading-5 font-medium text-gray-700
+					group-hover:text-gray-900">
+					Tom Cook
+				</p>
+				<p
+					class="text-xs leading-4 font-medium text-gray-500
+					group-hover:text-gray-700 transition ease-in-out duration-150">
+					View profile
+				</p>
+			</div>
+		</div>
+	</a>
+</div>
--- packages/client/src/pages/_layout.svelte
+++ packages/client/src/pages/_layout.svelte
@@ -1,8 +1,51 @@
-<ul>
-    <li><a href="/tab-1">Tab 1</a></li>
-    <li><a href="/tab-2">Tab 2</a></li>
-    <li><a href="/tab-3">Tab 3</a></li>
-    <li><a href="/tab-4">Tab 4</a></li>
-</ul>
+<script>
+	import { isActive, url, layout } from "@sveltech/routify";
 
-<slot />
+	import Home32 from 'carbon-icons-svelte/lib/Home32';
+	import Collaborate32 from 'carbon-icons-svelte/lib/Collaborate32';
+	import Folder32 from 'carbon-icons-svelte/lib/Folder32';
+	import Calendar32 from 'carbon-icons-svelte/lib/Calendar32';
+	import Email32 from 'carbon-icons-svelte/lib/Email32';
+	import ChartBar32 from 'carbon-icons-svelte/lib/ChartBar32';
+
+	import NavLink from '../components/ui/NavLink.svelte';
+	import Profile from '../components/ui/Profile.svelte'
+
+	const links = [
+		['./tab-2', 'Dashboard', Home32],
+		['./tab-1', 'Team', Collaborate32],
+		['./tab-1', 'Projects', Folder32],
+		['./tab-1', 'Calendar', Calendar32],
+		['./tab-1', 'Documents', Email32],
+		['./tab-1', 'Reports', ChartBar32],
+	]
+</script>
+
+<div class="h-screen flex overflow-hidden bg-gray-100">
+  <div class="flex flex-shrink-0">
+    <div class="flex flex-col w-64 border-r border-gray-200 bg-white">
+      <div class="h-0 flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
+        <div class="flex items-center flex-shrink-0 px-4">
+          <img class="h-8 w-auto" src="/img/logos/workflow-logo-on-white.svg" alt="Workflow" />
+        </div>
+        <nav class="mt-5 flex-1 px-2 bg-white">
+					{#each links as [path, name, Icon]}
+						<NavLink link={path} Icon={Icon}>{name}</NavLink>
+					{/each}
+        </nav>
+      </div>
+			<Profile />
+    </div>
+  </div>
+  <div class="flex flex-col w-0 flex-1 overflow-hidden">
+    <main class="flex-1 relative z-0 overflow-y-auto pt-2 pb-6 focus:outline-none py-6" tabindex="0">
+      <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+        <h1 class="text-2xl font-semibold text-gray-900">Dashboard
+        </h1>
+      </div>
+      <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+        <slot />
+      </div>
+    </main>
+  </div>
+</div>
